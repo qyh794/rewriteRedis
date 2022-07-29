2c4451a214b2280302a2a9e3be7bfaec4e1546af
qyh794 <71474241+qyh794@users.noreply.github.com>
2022-07-29 20:30:05 +0800

    database接口

--- interface/database/database.go
+++ interface/database/database.go
@@ -1,7 +1,9 @@
 package database
+
 import "rewriteRedis/interface/resp"
 
 type Database interface {
 	Exec(respConn resp.Connection, msg [][]byte) resp.Reply
 	Close()
+	AfterClientClose(respConn resp.Connection)
 }
