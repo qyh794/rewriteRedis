ceadf95c145f7c57289c1d87e021f90e4133d307
qyh794 <71474241+qyh794@users.noreply.github.com>
2022-07-31 20:10:26 +0800

    keys指令执行函数

---
+++ database/keys.go
@@ -0,0 +1,101 @@
+package database
+
+import (
+	"rewriteRedis/interface/resp"
+	"rewriteRedis/resp/reply"
+)
+
+// execDel del key1, key2...指令执行函数
+func execDel(db *DB, msg [][]byte) resp.Reply {
+	keys := make([]string, len(msg))
+	for i, v := range msg {
+		keys[i] = string(v)
+	}
+	deleted := db.RemoveKeys(keys...)
+	return reply.NewIntReply(int64(deleted))
+}
+
+// execExists exists key1, key2...指令执行函数,判断key是否存在
+func execExists(db *DB, msg [][]byte) resp.Reply {
+	res := int64(0)
+	for _, v := range msg {
+		key := string(v)
+		_, exists := db.GetEntity(key)
+		if exists {
+			res++
+		}
+	}
+	return reply.NewIntReply(res)
+}
+
+// execFlushDB 清空当前数据库
+func execFlushDB(db *DB, msg [][]byte) resp.Reply {
+	db.Flush()
+	return reply.NewOKReply()
+}
+
+// execType Type KEY执行执行函数
+func execType(db *DB, msg [][]byte) resp.Reply {
+	key := string(msg[0])
+	val, exists := db.GetEntity(key)
+	if !exists {
+		return reply.NewStatusReply("none")
+	}
+	switch val.(type) {
+	case []byte:
+		return reply.NewStatusReply("string")
+		// 后续实现list、hash
+	}
+	return reply.NewUnKnowErrReply()
+}
+
+// execRename rename oldKey newKey
+func execRename(db *DB, msg [][]byte) resp.Reply {
+	old := string(msg[0])
+	oldVal, exists := db.GetEntity(old)
+	if !exists {
+		return reply.NewStanderErrReply("ERR no such key")
+	}
+	newKey := string(msg[1])
+	db.Remove(old)
+	db.SetEntity(newKey, oldVal)
+	return reply.NewOKReply()
+}
+
+// execRenameNx RenameNx oldKey newKey,当newKey不存在时,将oldKey改名为newKey
+func execRenameNx(db *DB, msg [][]byte) resp.Reply {
+	old := string(msg[0])
+	val, exists := db.GetEntity(old)
+	if !exists {
+		return reply.NewStanderErrReply("ERR no such key")
+	}
+	newKey := string(msg[1])
+	_, exists = db.GetEntity(newKey)
+	if exists {
+		return reply.NewIntReply(int64(0))
+	}
+	db.Remove(old)
+	db.SetEntity(newKey, val)
+	return reply.NewIntReply(int64(1))
+}
+
+// execKeys returns all keys matching the given pattern
+func execKeys(db *DB, msg [][]byte) resp.Reply {
+	temp := db.KeysAll()
+	res := make([][]byte, 0)
+	for i := 0; i < len(temp); i++ {
+		res = append(res, []byte(temp[i]))
+	}
+	return reply.NewMultiBulkReply(res)
+}
+
+func init() {
+	RegisterCommand("DEL", execDel, -2)
+	RegisterCommand("EXISTS", execExists, -2)
+	RegisterCommand("FLUSHDB", execFlushDB, -1)
+	RegisterCommand("TYPE", execType, 2)
+	RegisterCommand("RENAME", execRename, 3)
+	RegisterCommand("RENAMENX", execRenameNx, 3)
+	RegisterCommand("KEYS", execKeys, 2)
+
+}
